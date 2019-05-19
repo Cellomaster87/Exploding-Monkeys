e9583c0489a65b4d829497c22d20fbb85b0ef85b
Michele Galvagno <michicello@gmail.com>
2019-05-19 14:36:05 +0200

    Setup and Building the Environment

---
+++ Exploding Monkeys/BuildingNode.swift
@@ -0,0 +1,74 @@
+//
+//  BuildingNode.swift
+//  Exploding Monkeys
+//
+//  Created by Michele Galvagno on 19/05/2019.
+//  Copyright © 2019 Michele Galvagno. All rights reserved.
+//
+
+import SpriteKit
+import UIKit
+
+class BuildingNode: SKSpriteNode {
+    var currentImage: UIImage!
+    
+    func setup() {
+        name = "building"
+        
+        currentImage = drawBuilding(size: size)
+        texture = SKTexture(image: currentImage)
+        
+        configurePhysics()
+    }
+    
+    func configurePhysics() {
+        physicsBody = SKPhysicsBody(texture: texture!, size: size)
+        physicsBody?.isDynamic = false
+        physicsBody?.categoryBitMask = CollisionTypes.building.rawValue
+        physicsBody?.contactTestBitMask = CollisionTypes.banana.rawValue
+    }
+    
+    func drawBuilding(size: CGSize) -> UIImage {
+        // 1. create a new Core Graphics context the size of our building
+        let renderer = UIGraphicsImageRenderer(size: size)
+        
+        let img = renderer.image { ctx in
+            // 2. fill it with a rectange that's one of three colors
+            let rectangle = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+            
+            let color: UIColor
+            
+            switch Int.random(in: 0...2) {
+            case 0:
+                color = UIColor(hue: 0.502, saturation: 0.98, brightness: 0.67, alpha: 1)
+            case 1:
+                color = UIColor(hue: 0.999, saturation: 0.99, brightness: 0.67, alpha: 1)
+            default:
+                color = UIColor(hue: 0, saturation: 0, brightness: 0.67, alpha: 1)
+            }
+            
+            color.setFill()
+            ctx.cgContext.addRect(rectangle)
+            ctx.cgContext.drawPath(using: .fill)
+            
+            // 3. draw windows all over the building in one of two colors: there's either a light on (yellow) or not (gray).
+            let lightOnColor = UIColor(hue: 0.19, saturation: 0.67, brightness: 0.99, alpha: 1)
+            let lightOffColor = UIColor(hue: 0, saturation: 0, brightness: 0.34, alpha: 1)
+            
+            for row in stride(from: 10, to: Int(size.height - 10), by: 40) {
+                for col in stride(from: 10, to: Int(size.width - 10), by: 40) {
+                    if Bool.random() {
+                        lightOnColor.setFill()
+                    } else {
+                        lightOffColor.setFill()
+                    }
+                    
+                    ctx.cgContext.fill(CGRect(x: col, y: row, width: 15, height: 20))
+                }
+            }
+        }
+        
+        // 4. pull out the result as a UIImage and return it for use elsewhere
+        return img
+    }
+}
